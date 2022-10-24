777d30468fed2a81bb79eefbc1be52f33ce16a18
minhhungle-ag <haless132@gmail.com>
2022-10-24 23:05:56 +0700

    update

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,2 +1,2 @@
 export const DEFAULT_THUMBNAIL = 'https://picsum.photos/id/279/1368/400'
-export const API_URL = 'https://heroku-post-ui.herokuapp.com/api'
+export const API_URL = 'https://heroku-post-ui-api.herokuapp.com/api' //https://heroku-post-ui.herokuapp.com/api
