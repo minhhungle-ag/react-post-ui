b7758d4ce85e9a6f09e96c5dea5fa6b4963f5444
minhhungle <haless132@gmail.com>
2023-03-11 09:38:57 +0700

    update

--- src/constants/common.js
+++ src/constants/common.js
@@ -2,6 +2,7 @@ export const DEFAULT_THUMBNAIL = 'https://picsum.photos/id/279/1368/400'
 export const API_URL = 'https://heroku-post-ui-server-production-8b8c.up.railway.app/api'
 
 export const CURRENT_LIMIT = 6
+
 export const USER = {
   avatar:
     'https://static.wixstatic.com/media/2e2a49_d456d06d41b346d3b36f426cb4142859~mv2.jpg/v1/fill/w_571,h_596,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/2e2a49_d456d06d41b346d3b36f426cb4142859~mv2.jpg',
