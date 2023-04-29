5df230575fd067c9b31f21ad75c75a2a61e3e44f
minhhungle <haless132@gmail.com>
2023-04-29 23:51:39 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-import { API_URL } from '../constants/common'
 
 const axiosClient = axios.create({
-  baseURL: API_URL, // process.evn.API_URL 'http://localhost:3000/api'
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
